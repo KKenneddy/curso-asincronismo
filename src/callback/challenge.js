f8700570a5b4c50122fa27a21fa22e6b46856050
Kennedy <nofuequequise@gmail.com>
2022-08-14 11:42:50 -0400

    Cap8

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -4,22 +4,36 @@
 //readyState === 3 'Iteractuando';
 //readyState === 4 'Completado';
 
-const XMLHttpRequest = require('xmlhttprequest');
+const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 const API = 'https://api.escuelajs.com/api/v1';
 
 function fechtData (urlApi, callback) {
     let xhttp = new XMLHttpRequest();
 
     xhttp.open('GET', urlApi, true);
+
     xhttp.onreadystatechange = function (event) {
         if (xhttp.readyState === 4) {
             if (xhttp.status === 200) {
-                callback(null, JSON.parse(xhttp.resposeText));
+                callback(null, JSON.parse(xhttp.responseText));
+            } else {
+                const error = new Error('Error' + urlApi);
+                return callback (error, null);
             }
-        } else {
-            const error = new Error('Error' + urlApi);
-            return callback (error, null);
         }
     }
     xhttp.send();
 }
+
+fechtData (`${API}/products`, function (error1, data1) {
+    if (error1) return console.error(error1);
+    fechtData (`${API}/products/${data1[0].id}`, function(error2, data2) {
+        if (error2) return console.error(error2);
+        fechtData(`${API}/categories/${data2?.category?.id}`, function(error3, data3) {
+            if (error3) return console.error(error3);
+            console.log(data1[0]);
+            console.log(data2.title);
+            console.log(data3.name);
+        });
+    });
+});
